5a553d28711655a0729c5c6fbe1fd0760e3760d7
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2020-12-11 13:42:08 -0500

    testing export

--- RandNum/RandNumScript.js
+++ RandNum/RandNumScript.js
@@ -3,14 +3,7 @@ const input2 = document.getElementById('int2');
 const button = document.getElementById('button');
 const randVal = document.getElementById('randVal');
 
-function gen() {
-    let int1 = Number(input1.value);
-    let int2 = Number(input2.value);
-    num = Math.floor(Math.random()*
-        (int2  - int1 + 1))
-    //get random number within range
-        randVal.innerHTML = num+int1;
-    //add minimum value to that number
+import gen from './RandNumScriptTest.js';
 
-}
+
 button.addEventListener('click', gen);
---
+++ RandNum/RandNumScriptTest.js
@@ -0,0 +1,12 @@
+
+export function gen() {
+    let int1 = Number(input1.value);
+    let int2 = Number(input2.value);
+    num = Math.floor(Math.random()*
+        (int2  - int1 + 1))
+    //get random number within range
+        randVal.innerHTML = num+int1;
+    //add minimum value to that number
+
+}
+
