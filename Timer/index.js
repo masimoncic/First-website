44f4c148d957040d733a73e5cb5a9b5a14b76d86
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2020-12-19 02:02:00 -0500

    a

--- Timer/index.js
+++ Timer/index.js
@@ -1,40 +1,38 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
 
-    start = () => {
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
 
-    }
+  pause = () => {
+    clearInterval(this.interval);
+  };
 
-    pause = () => {
-        clearInterval(this.interval);
-    }
+  tick = () => {
+    this.timeRemaining = this.timeRemaining - 1;
+  };
 
-    tick = () => {
-        this.timeRemaining = this.timeRemaining- 1;
-    }
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
 
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
 }
 
-const durationInput = document.getElementById("durationInput");
-const startButton = document.getElementById("startButton");
-const pauseButton = document.getElementById("pauseButton");
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
 
